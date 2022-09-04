d202e0f73060a0d73a7a59ac7951f8c7529d37df
sandy2030 <sandeeppandey2024@gmail.com>
2022-09-05 00:32:49 +0530

    concurrency updates

---
+++ src/com/concurrency/LockDemo.java
@@ -0,0 +1,7 @@
+package com.concurrency;
+
+/**
+ * Created by sande on 30-08-2022.
+ */
+public class LockDemo {
+}
---
+++ src/com/concurrency/ThreadBasicMethods_Demo.java
@@ -0,0 +1,18 @@
+package com.concurrency;
+
+/*
+*  demo of the methods of thread class and  its associated methods
+* */
+
+ public class ThreadBasicMethods_Demo {
+
+ // join
+
+ // yield
+
+ // wait
+
+ // sleep
+
+ //
+ }
